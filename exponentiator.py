24221ad4e20f9bf2dcc85f52aafd487cf428e81b
rich1123 <33075633+rich1123@users.noreply.github.com>
2020-03-04 17:52:10 -0500

    Add files via upload

--- exponentiator.py
+++ exponentiator.py
@@ -1,12 +1,24 @@
-def exponentiate(a,b):
+def exponentiate(a, b):
     return a ** b
 
+
+"""function for raising input number to a given power """
+
+
 def raise_to_fourth_power(a):
-    return exponentiate(a,4)
+    return exponentiate(a, 4)
+
+
+""""takes single param to pass through exponeniate function to give param power to 4th value"""
+
 
 square = lambda x: x ** 2;
 
 cube = lambda x: x ** 3;
+
+
+"""square and cube are lambdas(inline functions loaded into variables; scope isn't global"""
+
 
 print(square(2))
 print(cube(2))
--- greet.py
+++ greet.py
@@ -1,9 +1,10 @@
 def greet(name):
     print('Hello ' + name)
+"""The function that will take the name variable based on user input and print the response"""
 
 def name_input():
     name = input('Can I have your name please?\n')
     return name
-
+"""The function that asks the user the initial question and sets the name variable"""
 greet(name_input())
 
